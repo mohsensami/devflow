f41d52a130b75b5b066dbdbcafedc24d25ad5098
mohsensami <mohsensami18727@gmail.com>
2025-10-04 20:56:35 +0330

    Navbar

---
+++ components/navigation/navbar/index.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import Theme from "@/context/Theme";
+
+export default function Navbar() {
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/images/site-logo.svg"
+          width={23}
+          height={23}
+          alt="Dev Overflow Logo"
+        />
+
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Overflow</span>
+        </p>
+      </Link>
+      GlobalSearch
+      {/* <GlobalSearch /> */}
+      <div className="flex-between gap-5">
+        Theme
+        {/* <Theme /> */}
+        {/* {session?.user?.id && (
+          <UserAvatar
+            id={session.user.id}
+            name={session.user.name!}
+            imageUrl={session.user?.image}
+          />
+        )}
+
+        <MobileNavigation /> */}
+      </div>
+    </nav>
+  );
+}
