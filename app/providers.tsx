86e6fc28f8d37ca98a51cb839863f97d6704d5b3
mohsensami <mohsensami18727@gmail.com>
2025-10-06 20:17:25 +0330

    nextAuth setup

---
+++ app/providers.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import ThemeProvider from "@/context/Theme";
+
+type ProvidersProps = {
+  children: ReactNode;
+  session: any;
+};
+
+const Providers = ({ children, session }: ProvidersProps) => {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </SessionProvider>
+  );
+};
+
+export default Providers;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,35 +1,26 @@
 "use client";
 
 import Image from "next/image";
-// import { signIn } from "next-auth/react";
 import React from "react";
 
-import { Button } from "../ui/button";
+import { signIn } from "@/auth";
+import ROUTES from "@/constants/routes";
 
-// import ROUTES from "@/constants/routes";
-// import { toast } from "@/hooks/use-toast";
+import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 min-h-12 flex-1 rounded-2 px-4 py-3.5";
 
   const handleSignIn = async (provider: "github" | "google") => {
-    // try {
-    //   await signIn(provider, {
-    //     callbackUrl: ROUTES.HOME,
-    //     redirect: false,
-    //   });
-    // } catch (error) {
-    //   console.log(error);
-    //   toast({
-    //     title: "Sign-in Failed",
-    //     description:
-    //       error instanceof Error
-    //         ? error.message
-    //         : "An error occured during sign-in",
-    //     variant: "destructive",
-    //   });
-    // }
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
