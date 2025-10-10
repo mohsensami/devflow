55cf7fbecc68a2fb5529173744f4e32430afa7b1
mohsensami <mohsensami18727@gmail.com>
2025-10-10 09:22:51 +0330

    Left Sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
@@ -8,6 +9,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
       <Navbar />
 
       <div className="flex">
+        <LeftSidebar />
+
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
