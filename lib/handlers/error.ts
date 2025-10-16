7bca205eabca77739ff6a38c2c6b665a556c889f
mohsensami <mohsensami18727@gmail.com>
2025-10-16 12:26:02 +0330

    Implement Logging

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
+
 import { RequestError, ValidationError } from "../http-errors";
+import logger from "../logger";
+
 export type ResponseType = "api" | "server";
 
 const formatResponse = (
@@ -21,8 +24,14 @@ const formatResponse = (
     ? NextResponse.json(responseContent, { status })
     : { status, ...responseContent };
 };
+
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
+
     return formatResponse(
       responseType,
       error.statusCode,
@@ -35,6 +44,12 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
     const validationError = new ValidationError(
       error.flatten().fieldErrors as Record<string, string[]>
     );
+
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -44,8 +59,12 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
+
     return formatResponse(responseType, 500, error.message);
   }
+
+  logger.error({ err: error }, "An unexpected error occurred");
   return formatResponse(responseType, 500, "An unexpected error occurred");
 };
 
--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  serverExternalPackages: ["pino", "pino-pretty"],
   images: {
     remotePatterns: [
       {
