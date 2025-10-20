f3f80d4d769eab64a0b4f23c90f0a97adffb6387
mohsensami <mohsensami18727@gmail.com>
2025-10-20 18:33:46 +0330

    Implement Create Question Action

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,14 @@
-import QuestionForm from "@/components/forms/QuestionForm";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function AskAQuestion() {
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
+
+const AskQuestion = async () => {
+  const session = await auth();
+
+  if (!session) return redirect("/sign-in");
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
@@ -11,4 +18,6 @@ export default function AskAQuestion() {
       </div>
     </>
   );
-}
+};
+
+export default AskQuestion;
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,48 +1,33 @@
 interface Tag {
   _id: string;
-
   name: string;
 }
 
 interface Author {
   _id: string;
-
   name: string;
-
   image: string;
 }
 
 interface Question {
   _id: string;
-
   title: string;
-
   tags: Tag[];
-
   author: Author;
-
   createdAt: Date;
-
   upvotes: number;
-
   answers: number;
-
   views: number;
-
   createdAt: Date;
 }
 
 type ActionResponse<T = null> = {
   success: boolean;
-
   data?: T;
-
   error?: {
     message: string;
-
     details?: Record<string, string[]>;
   };
-
   status?: number;
 };
 
@@ -53,3 +38,8 @@ type ErrorResponse = ActionResponse<undefined> & { success: false };
 type APIErrorResponse = NextResponse<ErrorResponse>;
 
 type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
+
+interface RouteParams {
+  params: Promise<Record<string, string>>;
+  searchParams: Promise<Record<string, string>>;
+}
